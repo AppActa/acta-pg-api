66dfe557c2892a2c1d5ea70ac9d9596ed7cb369c
davi <davi.ramos@germinare.org.br>
2026-07-06 20:38:52 -0300

    fix/mapper: adiciona método para atualizar entidades e melhora a conversão de listas

--- src/main/java/br/com/acta/mapper/BaseMapper.java
+++ src/main/java/br/com/acta/mapper/BaseMapper.java
@@ -1,12 +1,17 @@
 package br.com.acta.mapper;
 
+import org.mapstruct.MappingTarget;
+
+import java.util.Collection;
 import java.util.List;
 
 public interface BaseMapper<REQ, RESP, ENT> {
     ENT toEntity(REQ dto);
     RESP toResponse(ENT ent);
+    void updateEntity(REQ dto, @MappingTarget ENT ent);
 
-    default List<RESP> toResponseList(List<ENT> entList){
+    default List<RESP> toResponseList(Collection<ENT> entList){
+        if (entList == null) return null;
         return entList.stream()
                 .map(this::toResponse)
                 .toList();
