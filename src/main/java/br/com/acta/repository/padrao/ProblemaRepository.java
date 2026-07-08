a03f7fef3ce92430d95f22709fbd4f4044390b03
davi <davi.ramos@germinare.org.br>
2026-07-07 21:46:53 -0300

    feat/env: adiciona configuração de datasource no application.properties e exemplo para variáveis de ambiente no .env.example

--- src/main/java/br/com/acta/repository/padrao/ProblemaRepository.java
+++ src/main/java/br/com/acta/repository/padrao/ProblemaRepository.java
@@ -3,7 +3,6 @@ package br.com.acta.repository.padrao;
 import br.com.acta.entity.pdca.Problema;
 import br.com.acta.repository.base.BaseRepository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface ProblemaRepository extends BaseRepository<Problema> {
