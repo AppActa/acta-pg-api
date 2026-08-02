895231f00475ac90c6300e87733a0ea18138c931
Davi Aliaga <kavhsao@gmail.com>
2026-08-02 15:19:10 -0300

    feat/service: adiciona AlertaPrazoService com métodos para gerenciar alertas de prazo e validações

--- src/main/java/br/com/acta/repository/padrao/AlertaPrazoRepository.java
+++ src/main/java/br/com/acta/repository/padrao/AlertaPrazoRepository.java
@@ -1,10 +1,13 @@
 package br.com.acta.repository.padrao;
 
 import br.com.acta.entity.pdca.AlertaPrazo;
+import br.com.acta.entity.pdca.Tarefa;
 import br.com.acta.repository.base.BaseRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AlertaPrazoRepository extends BaseRepository<AlertaPrazo> {
     List<AlertaPrazo> findByUsuarioDestinoIdAndLidoEmIsNull(Long idUsuario);
+    Optional<AlertaPrazo> findByTarefa(Tarefa tarefa);
 }
--- src/main/java/br/com/acta/utils/Validador.java
+++ src/main/java/br/com/acta/utils/Validador.java
@@ -2,11 +2,13 @@ package br.com.acta.utils;
 
 import br.com.acta.entity.core.Usuario;
 import br.com.acta.entity.enums.TipoUsuario;
+import br.com.acta.entity.join.UsuarioCiclo;
 import br.com.acta.entity.pdca.Ciclo;
 import br.com.acta.handler.exception.BusinessRuleException;
 import br.com.acta.handler.exception.ImmutableFieldException;
 import br.com.acta.handler.exception.InexistentFieldException;
 import java.util.Map;
+import java.util.Set;
 
 public final class Validador {
     public static void validarTipoUsuario(Usuario usuario, TipoUsuario... permitidos){
@@ -22,13 +24,25 @@ public final class Validador {
     /*
     public static void validarSubproblemas(Problema problema, Problema problemaPai){
         validarMesmoCiclo(problema.getCiclo(), problemaPai.getCiclo());
-
-
     }*/
 
     public static void validarMesmoCiclo(Ciclo a, Ciclo b) {
         if (!a.equals(b)) {
             throw new BusinessRuleException("As entidades devem pertencer ao mesmo ciclo");
+        }
+    }
+
+    public static void validarMesmoCiclo(Ciclo a, Set<UsuarioCiclo> ciclos) {
+        ciclos.forEach(ciclo -> validarMesmoCiclo(a, ciclo.getCiclo()));
+    }
+
+    public static void validarMesmoId(Long id1, Long id2, boolean deveSerIgual) {
+        if (deveSerIgual && !id1.equals(id2)) {
+            throw new BusinessRuleException("Os IDs das entidades devem ser iguais");
+        }
+
+        if (!deveSerIgual && id1.equals(id2)) {
+            throw new BusinessRuleException("Os IDs das entidades não devem ser iguais");
         }
     }
 
