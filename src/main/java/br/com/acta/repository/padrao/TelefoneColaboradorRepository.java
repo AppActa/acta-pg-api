4dac826e2c92d91892d9d7e20a703cb497752955
Davi Aliaga <kavhsao@gmail.com>
2026-08-06 06:50:16 -0300

    feat/contato: adiciona novos métodos de busca em repositórios de contato

--- src/main/java/br/com/acta/repository/padrao/TelefoneColaboradorRepository.java
+++ src/main/java/br/com/acta/repository/padrao/TelefoneColaboradorRepository.java
@@ -3,5 +3,9 @@ package br.com.acta.repository.padrao;
 import br.com.acta.entity.core.contato.TelefoneColaborador;
 import br.com.acta.repository.base.ContatoRepository;
 
+import java.util.List;
+
 public interface TelefoneColaboradorRepository extends ContatoRepository<TelefoneColaborador> {
+    List<TelefoneColaborador> findByColaborador_Id(Long id);
+    TelefoneColaborador findByColaboradorIdAndId(Long colaboradorId, Long id);
 }
