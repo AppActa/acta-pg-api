62b11d1b8c360b356ed0ebf288ad15c353d4d93a
Davi Aliaga <kavhsao@gmail.com>
2026-08-10 01:39:19 -0300

    feat/controller: implementa CicloController com endpoints para busca, criação, atualização parcial, exclusão e alteração de status de ciclos e ajusta CicloService e CicloRepository para suportar as operações

--- src/main/java/br/com/acta/repository/padrao/CicloRepository.java
+++ src/main/java/br/com/acta/repository/padrao/CicloRepository.java
@@ -3,13 +3,17 @@ package br.com.acta.repository.padrao;
 import br.com.acta.entity.enums.StatusCiclo;
 import br.com.acta.entity.pdca.Ciclo;
 import br.com.acta.repository.base.BaseRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CicloRepository extends BaseRepository<Ciclo> {
-    List<Ciclo> findByEmpresaId(Long idEmpresa);
-    List<Ciclo> findByGestorId(Long idGestor);
-    List<Ciclo> findByEmpresaIdAndStatus(Long idEmpresa, StatusCiclo status);
-    List<Ciclo> findByGestorIdAndStatus(Long idGestor, StatusCiclo status);
-    List<Ciclo> findByEmpresaIdAndGestorIdAndStatus(Long idEmpresa, Long idGestor, StatusCiclo status);
+    @Query("""
+    SELECT c FROM Ciclo c
+    WHERE (:idEmpresa IS NULL OR c.empresa.id = :idEmpresa)
+      AND (:idGestor IS NULL OR c.gestor.id = :idGestor)
+      AND (:status IS NULL OR c.status = :status)
+    """)
+    List<Ciclo> buscar(@Param("idEmpresa") Long idEmpresa, @Param("idGestor") Long idGestor, @Param("status") StatusCiclo status);
 }
