6fafcd4c4629ff5a6fe6d5ecd6908ddf0889e51f
Davi Aliaga <kavhsao@gmail.com>
2026-08-09 22:36:35 -0300

    feat/controller: implementa EmailController e TelefoneController com endpoints para gerenciar emails e telefones de empresas e colaboradores

--- src/main/java/br/com/acta/repository/padrao/TelefoneColaboradorRepository.java
+++ src/main/java/br/com/acta/repository/padrao/TelefoneColaboradorRepository.java
@@ -4,8 +4,9 @@ import br.com.acta.entity.core.contato.TelefoneColaborador;
 import br.com.acta.repository.base.ContatoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TelefoneColaboradorRepository extends ContatoRepository<TelefoneColaborador> {
     List<TelefoneColaborador> findByColaborador_Id(Long id);
-    TelefoneColaborador findByColaboradorIdAndId(Long colaboradorId, Long id);
+    Optional<TelefoneColaborador> findByColaboradorIdAndId(Long colaboradorId, Long id);
 }
