c8c5597e0956f25a4cb060f6d2e56b2103e2b307
Davi Aliaga <kavhsao@gmail.com>
2026-08-12 00:32:59 -0300

    refactor/repository: remove métodos findBy não utilizados e adiciona existsBy para otimização de consultas

--- src/main/java/br/com/acta/repository/padrao/UsuarioRepository.java
+++ src/main/java/br/com/acta/repository/padrao/UsuarioRepository.java
@@ -6,10 +6,8 @@ import br.com.acta.entity.enums.TipoUsuario;
 import br.com.acta.repository.base.BaseRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UsuarioRepository extends BaseRepository<Usuario> {
-    Optional<Usuario> findByEmailLogin(String emailLogin);
     boolean existsByEmailLoginIgnoreCase(String emailLogin);
     List<Usuario> findByTipoAndStatusAndEmpresaId(TipoUsuario tipo, StatusGeral status, Long empresaId);
     List<Usuario> findByEmpresaIdAndStatus(Long empresaId, StatusGeral status);
