0e6415bdd81c3c51ebf27c85ee693f5ed38999c5
Davi Aliaga <kavhsao@gmail.com>
2026-09-01 12:19:19 -0300

    feat/repository: adiciona métodos e procedimentos para gestão de prazo, ciclo e tarefa

--- src/main/java/br/com/acta/repository/padrao/TarefaRepository.java
+++ src/main/java/br/com/acta/repository/padrao/TarefaRepository.java
@@ -5,6 +5,7 @@ import br.com.acta.entity.enums.StatusTarefa;
 import br.com.acta.entity.pdca.Tarefa;
 import br.com.acta.repository.base.BaseRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
@@ -25,4 +26,10 @@ public interface TarefaRepository extends BaseRepository<Tarefa> {
             @Param("status") StatusTarefa statusTarefa,
             @Param("idResponsavel") Long idResponsavel,
             @Param("prioridade") Prioridade prioridade);
+
+    @Query(value = "SELECT pdca.fn_pode_iniciar_tarefa(:idTarefa, :idUsuario)", nativeQuery = true)
+    Boolean podeIniciarTarefa(@Param("idTarefa") Long idTarefa, @Param("idUsuario") Long idUsuario);
+
+    @Procedure(procedureName = "pdca.pr_reabrir_tarefa")
+    void reabrirTarefa(@Param("p_tarefa_id") Long tarefaId, @Param("p_novo_prazo") LocalDate novoPrazo);
 }
