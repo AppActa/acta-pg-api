fd2e400e629bfeb96f021211e02cb8c98211a284
Davi Aliaga <kavhsao@gmail.com>
2026-09-01 23:38:01 -0300

    feat/config: habilita agendamento na aplicação

--- src/main/java/br/com/acta/ActaPgApiApplication.java
+++ src/main/java/br/com/acta/ActaPgApiApplication.java
@@ -2,7 +2,9 @@ package br.com.acta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ActaPgApiApplication {
 
