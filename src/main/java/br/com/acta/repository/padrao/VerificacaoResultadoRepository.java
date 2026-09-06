9a5b0616e4ea8cf291d74e50726a5fb51feb2f49
Davi Aliaga <kavhsao@gmail.com>
2026-09-06 19:58:07 -0300

    feat/service: adiciona método getEntity em VerificacaoResultadoService e query customizada em VerificacaoResultadoRepository para validação de id da empresa do usuário autenticado

--- src/main/java/br/com/acta/repository/padrao/VerificacaoResultadoRepository.java
+++ src/main/java/br/com/acta/repository/padrao/VerificacaoResultadoRepository.java
@@ -5,7 +5,10 @@ import br.com.acta.entity.pdca.VerificacaoResultado;
 import br.com.acta.repository.base.BaseRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VerificacaoResultadoRepository extends BaseRepository<VerificacaoResultado> {
     List<VerificacaoResultado> findByCiclo(Ciclo ciclo);
+
+    Optional<VerificacaoResultado> findByIdAndCicloEmpresaId(Long id, Long cicloEmpresaId);
 }
