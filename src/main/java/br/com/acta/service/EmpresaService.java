d6362d9215f57102c8c0b7f1bdf8da1b8394a621
Davi Aliaga <kavhsao@gmail.com>
2026-09-17 10:36:25 -0300

    refactor(services): remove gerenciamento de e-mail, telefone e endereço de EmpresaService e ColaboradorService

--- src/main/java/br/com/acta/service/EmpresaService.java
+++ src/main/java/br/com/acta/service/EmpresaService.java
@@ -8,27 +8,16 @@ import br.com.acta.common.utils.ConversorObject;
 import br.com.acta.common.utils.PatchConfig;
 import br.com.acta.common.utils.Validador;
 import br.com.acta.dto.core.contato.email.EmailRequestDTO;
-import br.com.acta.dto.core.contato.email.EmailResponseDTO;
 import br.com.acta.dto.core.contato.telefone.TelefoneRequestDTO;
-import br.com.acta.dto.core.contato.telefone.TelefoneResponseDTO;
 import br.com.acta.dto.core.empresa.EmpresaRequestDTO;
 import br.com.acta.dto.core.empresa.EmpresaResponseDTO;
-import br.com.acta.dto.core.empresa.endereco.EnderecoRequestDTO;
-import br.com.acta.dto.core.empresa.endereco.EnderecoResponseDTO;
 import br.com.acta.dto.core.empresa.EmpresaMapper;
-import br.com.acta.dto.core.empresa.endereco.EnderecoMapper;
-import br.com.acta.dto.core.contato.email.EmailEmpresaMapper;
-import br.com.acta.dto.core.contato.telefone.TelefoneEmpresaMapper;
 import br.com.acta.entity.core.Empresa;
-import br.com.acta.entity.core.Endereco;
-import br.com.acta.entity.core.contato.EmailEmpresa;
-import br.com.acta.entity.core.contato.TelefoneEmpresa;
 import br.com.acta.entity.enums.StatusCiclo;
 import br.com.acta.entity.enums.StatusGeral;
 import br.com.acta.entity.enums.TamanhoEmpresa;
 import br.com.acta.repository.padrao.EmailEmpresaRepository;
 import br.com.acta.repository.padrao.EmpresaRepository;
-import br.com.acta.repository.padrao.EnderecoRepository;
 import br.com.acta.repository.padrao.TelefoneEmpresaRepository;
 import br.com.acta.service.base.BaseService;
 import br.com.caelum.stella.validation.CNPJValidator;
@@ -45,12 +34,8 @@ public class EmpresaService
 extends BaseService<EmpresaRequestDTO, EmpresaResponseDTO, Empresa> {
     private final EmpresaRepository repo;
     private final EmpresaMapper mapper;
-    private final EmailEmpresaMapper emailMapper;
     private final EmailEmpresaRepository emailRepo;
-    private final TelefoneEmpresaMapper telefoneMapper;
     private final TelefoneEmpresaRepository telefoneRepo;
-    private final EnderecoMapper enderecoMapper;
-    private final EnderecoRepository enderecoRepo;
     private final PatchConfig patchConfig = new PatchConfig(
             Set.of("cnpj", "nome", "tamanho", "setor", "status"),
             Set.of("nome", "tamanho", "setor")
@@ -63,16 +48,12 @@ extends BaseService<EmpresaRequestDTO, EmpresaResponseDTO, Empresa> {
                 .orElseThrow(() -> new ModelNotFoundException("Empresa", id));
     }
 
-    public EmpresaService(EmpresaRepository repo, EmpresaMapper mapper, EmailEmpresaMapper emailMapper, EmailEmpresaRepository emailRepo, TelefoneEmpresaMapper telefoneMapper, TelefoneEmpresaRepository telefoneRepo, EnderecoMapper enderecoMapper, EnderecoRepository enderecoRepo, AuthService authService) {
+    public EmpresaService(EmpresaRepository repo, EmpresaMapper mapper, EmailEmpresaRepository emailRepo, TelefoneEmpresaRepository telefoneRepo, AuthService authService) {
         super(repo, mapper, authService);
         this.repo = repo;
         this.mapper = mapper;
-        this.emailMapper = emailMapper;
         this.emailRepo = emailRepo;
-        this.telefoneMapper = telefoneMapper;
         this.telefoneRepo = telefoneRepo;
-        this.enderecoMapper = enderecoMapper;
-        this.enderecoRepo = enderecoRepo;
     }
 
     @PreAuthorize("hasRole('ADMIN') and authService.isUsuarioByIdEmpresa(#id)")
@@ -148,120 +129,6 @@ extends BaseService<EmpresaRequestDTO, EmpresaResponseDTO, Empresa> {
         for (TelefoneRequestDTO telefone : dto.telefones()) {
             if (telefoneRepo.existsByContatoIgnoreCase(telefone.numero())) throw new UniqueViolationException("Telefone");
         }
-    }
-
-    @PreAuthorize("isAuthenticated()")
-    @Transactional(readOnly = true)
-    public List<EmailResponseDTO> buscarEmails(Long idEmpresa){
-        Empresa empresa = getEntity(idEmpresa);
-        List<EmailEmpresa> email = emailRepo.findByEmpresa_Id(empresa.getId());
-
-        return emailMapper.toResponseList(email);
-    }
-
-    @PreAuthorize("hasRole('ADMIN') and authService.isUsuarioByIdEmpresa(#idEmpresa)")
-    @Transactional
-    public EmailResponseDTO inserirEmail(Long idEmpresa, EmailRequestDTO dto){
-        configurarUsuarioAtual();
-        Empresa empresa = getEntity(idEmpresa);
-
-        if (emailRepo.existsByEmpresaIdAndContatoIgnoreCase(idEmpresa, dto.email())) throw new UniqueViolationException("E-mail");
-
-        EmailEmpresa email = emailMapper.toEntity(dto);
-        email.setEmpresa(empresa);
-
-        EmailEmpresa salvo = emailRepo.save(email);
-        return emailMapper.toResponse(salvo);
-    }
-
-    @PreAuthorize("hasRole('ADMIN') and authService.isUsuarioByIdEmpresa(#idEmpresa)")
-    @Transactional
-    public void excluirEmail(Long idEmpresa, Long idEmail){
-        configurarUsuarioAtual();
-        EmailEmpresa email = getEmail(idEmpresa, idEmail);
-        emailRepo.delete(email);
-    }
-
-    @PreAuthorize("isAuthenticated()")
-    @Transactional(readOnly = true)
-    public List<TelefoneResponseDTO> buscarTelefones(Long idEmpresa){
-        Empresa empresa = getEntity(idEmpresa);
-        List<TelefoneEmpresa> telefone = telefoneRepo.findByEmpresa_Id(empresa.getId());
-
-        return telefoneMapper.toResponseList(telefone);
-    }
-
-    @PreAuthorize("hasRole('ADMIN') and authService.isUsuarioByIdEmpresa(#idEmpresa)")
-    @Transactional
-    public TelefoneResponseDTO inserirTelefone(Long idEmpresa, TelefoneRequestDTO dto){
-        configurarUsuarioAtual();
-        Empresa empresa = getEntity(idEmpresa);
-        TelefoneEmpresa telefone = telefoneMapper.toEntity(dto);
-
-        if (telefoneRepo.existsByEmpresaIdAndContato(idEmpresa, dto.numero())) throw new UniqueViolationException("Telefone");
-
-        telefone.setEmpresa(empresa);
-
-        TelefoneEmpresa salvo = telefoneRepo.save(telefone);
-        return telefoneMapper.toResponse(salvo);
-    }
-
-    @PreAuthorize("hasRole('ADMIN') and authService.isUsuarioByIdEmpresa(#idEmpresa)")
-    @Transactional
-    public void excluirTelefone(Long idEmpresa, Long idTelefone){
-        configurarUsuarioAtual();
-        TelefoneEmpresa telefone = getTelefone(idEmpresa, idTelefone);
-        telefoneRepo.delete(telefone);
-    }
-
-    @PreAuthorize("isAuthenticated()")
-    @Transactional(readOnly = true)
-    public EnderecoResponseDTO buscarEndereco(Long idEmpresa, Long idEndereco) {
-        Endereco endereco = getEndereco(idEmpresa, idEndereco);
-        return enderecoMapper.toResponse(endereco);
-    }
-
-    @PreAuthorize("isAuthenticated()")
-    @Transactional(readOnly = true)
-    public List<EnderecoResponseDTO> buscarEndereco(Long idEmpresa) {
-        Empresa empresa = getEntity(idEmpresa);
-        List<Endereco> enderecos = empresa.getEnderecos();
-        return enderecoMapper.toResponseList(enderecos);
-    }
-
-    @PreAuthorize("hasRole('ADMIN') and authService.isUsuarioByIdEmpresa(#idEmpresa)")
-    @Transactional
-    public EnderecoResponseDTO inserirEndereco(Long idEmpresa, EnderecoRequestDTO dto) {
-        configurarUsuarioAtual();
-        Empresa empresa = getEntity(idEmpresa);
-        Endereco endereco = enderecoMapper.toEntity(dto);
-
-        endereco.setEmpresa(empresa);
-        Endereco salvo = enderecoRepo.save(endereco);
-        return enderecoMapper.toResponse(salvo);
-    }
-
-    @PreAuthorize("hasRole('ADMIN') and authService.isUsuarioByIdEmpresa(#idEmpresa)")
-    @Transactional
-    public void excluirEndereco(Long idEmpresa, Long idEndereco) {
-        configurarUsuarioAtual();
-        Endereco endereco = getEndereco(idEmpresa, idEndereco);
-        enderecoRepo.delete(endereco);
-    }
-
-    private Endereco getEndereco(Long idEmpresa, Long idEndereco){
-        Empresa empresa = getEntity(idEmpresa);
-        return enderecoRepo.findByEmpresaAndId(empresa, idEndereco).orElseThrow(() -> new ModelNotFoundException("Endereco", idEndereco));
-    }
-
-    private EmailEmpresa getEmail(Long idEmpresa, Long idEmail){
-        Empresa empresa = getEntity(idEmpresa);
-        return emailRepo.findByEmpresaIdAndId(empresa.getId(), idEmail).orElseThrow(() -> new ModelNotFoundException("Email", idEmail));
-    }
-
-    private TelefoneEmpresa getTelefone(Long idEmpresa, Long idTelefone){
-        Empresa empresa = getEntity(idEmpresa);
-        return telefoneRepo.findByEmpresaIdAndId(empresa.getId(), idTelefone).orElseThrow(() -> new ModelNotFoundException("Telefone", idTelefone));
     }
 
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
