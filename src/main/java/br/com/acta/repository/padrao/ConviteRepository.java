8162ee3123e0747b46db79c300239d1d9065dfa2
Davi Aliaga <kavhsao@gmail.com>
2026-09-18 01:20:45 -0300

    refactor/repository: ajusta método findByTokenHash para carregar relacionamentos relevantes

--- src/main/java/br/com/acta/repository/padrao/ConviteRepository.java
+++ src/main/java/br/com/acta/repository/padrao/ConviteRepository.java
@@ -1,13 +1,19 @@
 package br.com.acta.repository.padrao;
 
 import br.com.acta.entity.core.Convite;
-import br.com.acta.entity.enums.StatusConvite;
 import br.com.acta.repository.base.BaseRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ConviteRepository extends BaseRepository<Convite> {
+
+    @Query("""
+        SELECT c FROM Convite c
+        JOIN FETCH c.usuarioDestino u
+        JOIN FETCH u.empresa
+        LEFT JOIN FETCH u.colaborador
+        WHERE c.tokenHash = :tokenHash
+        """)
     Optional<Convite> findByTokenHash(String tokenHash);
-    boolean existsByUsuarioDestinoIdAndStatus(Long idUsuario, StatusConvite status);
-    Optional<Convite> findByUsuarioDestinoIdAndStatus(Long usuarioDestinoId, StatusConvite status);
 }
